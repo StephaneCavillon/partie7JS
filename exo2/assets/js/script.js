903a011351a9ce5610c41f70037ae3cf19f4ffb1
StephaneC <stephane.cavillon@itech.fr>
2020-11-12 12:27:42 +0100

    fin exo2

---
+++ exo2/assets/js/script.js
@@ -0,0 +1,39 @@
+//Déclaration des variables
+let div = document.querySelector('div');
+let btnBold = document.querySelector('#btnBold');
+let btnColor = document.querySelector('#btnColor');
+let btnEnlarge = document.querySelector('#btnEnlarge');
+
+
+// Création des Listeners
+btnBold.addEventListener('click', bold);
+btnColor.addEventListener('click', colorChange);
+btnEnlarge.addEventListener('click', enlarge);
+
+
+// Création des fonctions
+
+// fonction mettre en gras
+function bold(){
+    if(div.style.fontWeight == 'bold'){
+        div.style.fontWeight = 'initial';
+    } else{
+        div.style.fontWeight = 'bold';
+    };
+};
+
+//fonction changer de couleur
+function colorChange(){
+    div.style.color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+};
+
+//fonction agrandir
+function enlarge(){
+    if(div.style.fontSize == 'large'){
+    div.style.fontSize = '';
+    } else {
+        div.style.fontSize = 'large'
+    };
+};
+
+
