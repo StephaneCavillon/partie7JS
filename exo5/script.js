872bb39762d1db1949a07d9bfe7e86f629cdd7f7
StephaneC <stephane.cavillon@itech.fr>
2020-11-13 09:29:35 +0100

    fin exo6

--- exo5/script.js
+++ exo5/script.js
@@ -14,11 +14,12 @@ function getStringLength(string){
 
   let getStringlength = (string) => {
       let stringLength;
-      if(string.length ===1){
+      if(string.length === 1){
           stringLength = 'La chaîne contient qu\'un seul caractère';
       } else {
         stringLength = `La chaîne contient ${string.length} caractères`;
       }
+      // pas sur que le return soit necessaire
       return stringLength;
   }
 
---
+++ exo6/script.js
@@ -0,0 +1,9 @@
+(name) => {
+    `Bonjour, ${name} ! Comment vas-tu ?`;
+  }
+
+function disBonjour(name){
+    let bonjour = `Bonjour, ${name} ! Comment vas-tu ?`;
+
+    return bonjour; 
+}
