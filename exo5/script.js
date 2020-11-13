b4b1eb8a54fc18b8c70996ca1ed80d67b75f25e7
StephaneC <stephane.cavillon@itech.fr>
2020-11-13 09:24:37 +0100

    fin exo5

---
+++ exo5/script.js
@@ -0,0 +1,25 @@
+
+
+
+function getStringLength(string){
+    let stringLength;
+    if(string.length === 1){
+      stringLength = 'La chaîne contient qu\'un seul caractère';
+    } else {
+      stringLength = `La chaîne contient ${string.length} caractères`;
+    }
+    return stringLength;
+  }
+  
+
+  let getStringlength = (string) => {
+      let stringLength;
+      if(string.length ===1){
+          stringLength = 'La chaîne contient qu\'un seul caractère';
+      } else {
+        stringLength = `La chaîne contient ${string.length} caractères`;
+      }
+      return stringLength;
+  }
+
+  console.log(getStringLength);
