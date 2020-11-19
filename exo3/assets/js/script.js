f64d4625f6f39c0b69b23b03e02248db00902f57
StephaneC <stephane.cavillon@itech.fr>
2020-11-19 12:16:36 +0100

    fin correction

--- exo3/assets/js/script.js
+++ exo3/assets/js/script.js
@@ -1,10 +1,11 @@
 // déclaration des variables
-let number1 = document.getElementById('number1').value;
-let number2 = document.getElementById('number2').value;
 let resultbtn = document.getElementById('btnmult');
 
 //création des listener
 resultbtn.addEventListener('click', () => {
+    let number1 = document.getElementById('number1').value;
+    let number2 = document.getElementById('number2').value;
+
     alert(number1*number2);
 });
 
